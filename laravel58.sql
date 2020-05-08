b57b051d391149826421ca29e66502c559bd7fd5
justForLu <1120714124@qq.com>
2020-05-08 12:59:02 +0800

    edit:SQL文件更新

--- laravel58.sql
+++ laravel58.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 19/02/2020 13:17:48
+ Date: 08/05/2020 11:57:40
 */
 
 SET NAMES utf8mb4;
@@ -34,7 +34,7 @@ CREATE TABLE `ss_category` (
   `delete_time` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COMMENT='网站分类表';
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COMMENT='网站分类表';
 
 -- ----------------------------
 -- Records of ss_category
@@ -42,6 +42,10 @@ CREATE TABLE `ss_category` (
 BEGIN;
 INSERT INTO `ss_category` VALUES (1, '行业新闻', 0, 1, 0, 1, '', 1581737949, 0, 0);
 INSERT INTO `ss_category` VALUES (2, '公司新闻', 0, 1, 0, 1, '', 1581737974, 1581738606, 0);
+INSERT INTO `ss_category` VALUES (7, '测试', 0, 1, 0, 1, '', 1588863479, 1588863479, 0);
+INSERT INTO `ss_category` VALUES (8, 'ces', 0, 1, 0, 1, '', 1588863768, 1588863768, 0);
+INSERT INTO `ss_category` VALUES (9, 'ces', 0, 1, 0, 1, '', 1588863856, 1588863856, 0);
+INSERT INTO `ss_category` VALUES (10, 'ces', 0, 1, 0, 1, '', 1588864510, 1588864510, 0);
 COMMIT;
 
 -- ----------------------------
@@ -105,6 +109,12 @@ CREATE TABLE `ss_file` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='上传文件表';
 
 -- ----------------------------
+-- Records of ss_file
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
 -- Table structure for ss_link
 -- ----------------------------
 DROP TABLE IF EXISTS `ss_link`;
@@ -112,6 +122,38 @@ CREATE TABLE `ss_link` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='友情链接表';
+
+-- ----------------------------
+-- Records of ss_link
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for ss_log
+-- ----------------------------
+DROP TABLE IF EXISTS `ss_log`;
+CREATE TABLE `ss_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT '0' COMMENT '管理员id',
+  `operate_module` varchar(50) DEFAULT '' COMMENT '请求控制器',
+  `operate_action` varchar(50) DEFAULT '' COMMENT '请求方法',
+  `operate_url` varchar(200) DEFAULT '' COMMENT '请求URL',
+  `content` varchar(255) DEFAULT '' COMMENT '日志内容',
+  `ip` varchar(20) DEFAULT '' COMMENT 'IP',
+  `module` tinyint(1) DEFAULT '0' COMMENT '前、后台',
+  `create_time` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of ss_log
+-- ----------------------------
+BEGIN;
+INSERT INTO `ss_log` VALUES (1, 1, 'category', 'store', '/admin/category', '新增分类', '127.0.0.1', 1, 1588863479);
+INSERT INTO `ss_log` VALUES (2, 1, 'login', 'login', '/admin/login', '登录后台', '127.0.0.1', 1, 1588866742);
+INSERT INTO `ss_log` VALUES (3, 1, 'login', 'login', '/admin/login', '登录后台', '127.0.0.1', 1, 1588906078);
+COMMIT;
 
 -- ----------------------------
 -- Table structure for ss_manager
@@ -138,8 +180,8 @@ CREATE TABLE `ss_manager` (
 -- Records of ss_manager
 -- ----------------------------
 BEGIN;
-INSERT INTO `ss_manager` VALUES (1, 'admin', '$2y$10$K9ZqSDPveI6zuQjOgJj3OeWibeAevedhv5E6vOSLCo2qizF1GAUw.', 'w5FkrwnY2zzxs1i1lOONfVYU98Bv6wJF5ADOmEXWyoV8JVN5MIcTOtMEcrsn', '2020-02-19 09:54:30', '127.0.0.1', 1, '0,1,1,', 1, 1, NULL, '2020-02-19 09:54:30', NULL);
-INSERT INTO `ss_manager` VALUES (2, 'test11', '$2y$10$Ujyg8YXL9SGVaqNFf9slbOwNhaWBO3hqvpMEpLozqM8u6E7vP9hZK', 'PtmQwtKVTFPenmidu7NxC7DF1NzOn5DojN2vxkfzIL9WBw6nSfavItdQZOCQ', '2020-02-13 09:46:46', '127.0.0.1', 1, '0,1,1,2,', 1, 0, NULL, '2020-02-19 09:54:43', NULL);
+INSERT INTO `ss_manager` VALUES (1, 'admin', '$2y$10$K9ZqSDPveI6zuQjOgJj3OeWibeAevedhv5E6vOSLCo2qizF1GAUw.', 'dLzNcAgSABMrvLoWIo1T9jrgiS6cBP4z9Hf7B6IORhvGHDp9UltycumZZmpf', '2020-05-08 10:47:58', '127.0.0.1', 1, '0,1,1,', 1, 1, '2020-02-19 09:55:12', '2020-05-08 10:47:58', NULL);
+INSERT INTO `ss_manager` VALUES (2, 'test11', '$2y$10$Ujyg8YXL9SGVaqNFf9slbOwNhaWBO3hqvpMEpLozqM8u6E7vP9hZK', 'PtmQwtKVTFPenmidu7NxC7DF1NzOn5DojN2vxkfzIL9WBw6nSfavItdQZOCQ', '2020-02-13 09:46:46', '127.0.0.1', 1, '0,1,1,2,', 1, 0, '2020-02-19 09:55:12', '2020-02-19 09:54:43', NULL);
 INSERT INTO `ss_manager` VALUES (3, 'ssss', '$2y$10$w4kWVn6/JZrnziMkV0/bJ.KoAfAIrN9vAXtOFO5ZQh9mjfQoK.jhm', '', NULL, '', 1, '0,1,1,3,', 1, 0, '2020-02-19 09:55:12', '2020-02-19 09:55:12', NULL);
 COMMIT;
 
@@ -160,7 +202,7 @@ CREATE TABLE `ss_menu` (
   `module` tinyint(4) NOT NULL DEFAULT '0' COMMENT '所属模块',
   `is_system` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否系统菜单',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';
 
 -- ----------------------------
 -- Records of ss_menu
@@ -174,6 +216,7 @@ INSERT INTO `ss_menu` VALUES (5, '菜单管理', 'menu', 1, '0,1,5', '/menu', 2,
 INSERT INTO `ss_menu` VALUES (6, '网站配置', 'config', 1, '0,1,1,15', '/config', 2, 5, 1, 1, 0);
 INSERT INTO `ss_menu` VALUES (9, '分类管理', 'category', 1, '0,1,1,19', '/category', 2, 9, 1, 1, 0);
 INSERT INTO `ss_menu` VALUES (10, '反馈管理', 'feedback', 1, '0,1,1,20', '/feedback', 2, 10, 1, 1, 0);
+INSERT INTO `ss_menu` VALUES (11, '日志列表', 'log', 1, '0,1,1,11', '/log', 2, 11, 1, 1, 0);
 COMMIT;
 
 -- ----------------------------
@@ -190,7 +233,7 @@ CREATE TABLE `ss_permission` (
   `is_system` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否系统权限',
   PRIMARY KEY (`id`),
   UNIQUE KEY `wx_permissions_code_unique` (`code`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
 
 -- ----------------------------
 -- Records of ss_permission
@@ -219,8 +262,6 @@ INSERT INTO `ss_permission` VALUES (20, '角色删除', 'role.destroy', '', 3, 1
 INSERT INTO `ss_permission` VALUES (21, '角色授权', 'role.authority', '', 3, 1, 0);
 INSERT INTO `ss_permission` VALUES (22, '网站配置', 'config.index', '', 6, 1, 0);
 INSERT INTO `ss_permission` VALUES (23, '网站配置修改', 'config.update', '', 6, 1, 0);
-INSERT INTO `ss_permission` VALUES (24, '关于我们', 'about.index', '', 7, 1, 0);
-INSERT INTO `ss_permission` VALUES (25, '关于我们修改', 'about.update', '', 7, 1, 0);
 INSERT INTO `ss_permission` VALUES (26, '分类列表', 'category.index', ' ', 9, 1, 0);
 INSERT INTO `ss_permission` VALUES (27, '分类新增', 'category.create,category.store', ' ', 9, 1, 0);
 INSERT INTO `ss_permission` VALUES (28, '分类编辑', 'category.edit,category.update', ' ', 9, 1, 0);
@@ -228,6 +269,7 @@ INSERT INTO `ss_permission` VALUES (29, '分类删除', 'category.destroy', ' ',
 INSERT INTO `ss_permission` VALUES (30, '反馈列表', 'feedback.index', ' ', 10, 1, 0);
 INSERT INTO `ss_permission` VALUES (31, '反馈处理', 'feedback.edit,feedback.update', ' ', 10, 1, 0);
 INSERT INTO `ss_permission` VALUES (32, '反馈删除', 'feedback.destroy', ' ', 10, 1, 0);
+INSERT INTO `ss_permission` VALUES (33, '日志列表', 'log.index', '', 11, 1, 0);
 COMMIT;
 
 -- ----------------------------
@@ -246,15 +288,6 @@ CREATE TABLE `ss_permission_role` (
 -- Records of ss_permission_role
 -- ----------------------------
 BEGIN;
-INSERT INTO `ss_permission_role` VALUES (20, 16, 3, 1);
-INSERT INTO `ss_permission_role` VALUES (21, 17, 3, 1);
-INSERT INTO `ss_permission_role` VALUES (22, 18, 3, 1);
-INSERT INTO `ss_permission_role` VALUES (23, 19, 3, 1);
-INSERT INTO `ss_permission_role` VALUES (24, 20, 3, 1);
-INSERT INTO `ss_permission_role` VALUES (25, 21, 3, 1);
-INSERT INTO `ss_permission_role` VALUES (26, 6, 2, 1);
-INSERT INTO `ss_permission_role` VALUES (27, 16, 2, 1);
-INSERT INTO `ss_permission_role` VALUES (28, 17, 2, 1);
 COMMIT;
 
 -- ----------------------------
