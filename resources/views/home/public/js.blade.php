77a50c09fafdd019cca84768b8aa62c2770ffc2e
justForLu <1120714124@qq.com>
2021-02-26 18:10:02 +0800

    首页跑马灯

--- resources/views/home/public/js.blade.php
+++ resources/views/home/public/js.blade.php
@@ -1,25 +1,12 @@
 <script src="{{asset("/assets/home/js/jquery.js")}}"></script>
 <script src="{{asset("/assets/home/js/iealert.js")}}"></script>
 <script src="{{asset("/assets/home/js/ckbrowser.js")}}"></script>
-{{--<script src="{{asset("/assets/home/js/layer.js")}}"></script>--}}
 <script src="{{asset("/assets/home/js/main.js")}}"></script>
 <script src="{{asset("/assets/home/js/swiper.js")}}"></script>
 <script src="{{asset("/assets/home/js/Popt.js")}}"></script>
 <script src="{{asset("/assets/home/js/citySet.js")}}"></script>
-<script src="{{asset("/assets/home/js/jquery-3.2.1.min.js")}}"></script>
 <script src="{{asset("/assets/plugins/validate/Validform_v5.3.2_min.js")}}"></script>
 <script src="{{asset("/assets/plugins/layer/layer.js")}}"></script>
 <script src="{{asset("/assets/home/js/form.js")}}"></script>
-{{--<script src="{{asset("/assets/home/js/tvp.player_v2_jq.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/index.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/fac.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/hm.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/lazyload.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/login.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/md5.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/news.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/Popt.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/push.js")}}"></script>--}}
-{{--<script src="{{asset("/assets/home/js/reg.js")}}"></script>--}}
 
 
